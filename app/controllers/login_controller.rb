40e572cb9a94aaf7338b48821e162b4cc655d8c6
LuisitoRady <[crdluis97@.gmail.com]>
2018-11-28 22:13:33 -0600

    Add @ to user in login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,10 +7,10 @@ class LoginController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+  	@user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      log_in user
+      log_in @user
       redirect_to root_path
     else
       redirect_to login_path, danger: "El correo/contraseña son inválidos"
